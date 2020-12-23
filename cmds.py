43c1bb2e9145bcdd21ca3b5059337e0022a58544
John Madon <john@unwisegeek.net>
2020-12-23 03:54:42 -0500

    Moved large-scale player command handling to cmds module

--- cmds.py
+++ cmds.py
@@ -1,14 +1,29 @@
-def look(room, roomnum):
-    # Show the room name, description, and exits to players.
-    exitstr = ""
-    exit_list = ["n", "s", "e", "w"]
-    for exit in exit_list:
-        if room[roomnum][exit]["leadsto"] != -1:
-            if room[roomnum][exit]["isclosed"]:
-                exitstr += "<{}> ".format(exit)
-            else:
-                exitstr += "{} ".format(exit)
+# All possible player commands must fall under the dispatcher function
+# Returns True or False depending on if main loop should continue or wait for
+# additional player input.
+def dispatcher(room, player, command):
 
-    print("{}\n{}\n\nExits: {}\n".format(room[roomnum]['name'],
-                                         room[roomnum]['desc'],
-                                         exitstr))
+    def look(room, player, args):
+        # Show the room name, description, and exits to players.
+        roomnum = player["location"]
+        exitstr = ""
+        exit_list = ["n", "s", "e", "w"]
+        for exit in exit_list:
+            if room[roomnum][exit]["leadsto"] != -1:
+                if room[roomnum][exit]["isclosed"]:
+                    exitstr += "<{}> ".format(exit)
+                else:
+                    exitstr += "{} ".format(exit)
+
+        print("{}\n{}\n\nExits: {}\n".format(room[roomnum]['name'],
+                                             room[roomnum]['desc'],
+                                             exitstr))
+        return False
+
+    index = { "look": look }
+
+    if command[0] in index:
+        return index[command[0]](room, player, command[1:])
+    else:
+        print("Command not found.")
+        return False
--- main.py
+++ main.py
@@ -2,41 +2,44 @@ from rooms import room
 import cmds
 
 # Initial Variables
-roomnum = 0
-keyboardinput = " "
-global room
-
+command = ""
+player = { "location": 0
+}
 
 def exithandler(num, exitdir):
     if room[num][exitdir]["leadsto"] != -1:
         return room[num][exitdir]["leadsto"]
     return -1
 
-
-while "x" not in keyboardinput.lower():
+while "x" not in command:
     continue_loop = False
-    cmds.look(room, roomnum)
+    cmds.dispatcher(room, player, ["look"])
 
     while not continue_loop:
-        keyboardinput = input("Command: ")
+        command = input("Command: ").lower()
 
-        if keyboardinput.lower() == "":
+        if command == "":
             continue
 
+        # Command handlers
+        if command.split(' ')[0]:
+            if command[0] not in "nsew":
+                continue_loop = cmds.dispatcher(room, player, command.split(' '))
+
         # Exit handlers
-        if keyboardinput.lower() in "nsew":
-            dir_of_travel = keyboardinput.lower()
-            new_roomnum = exithandler(roomnum, dir_of_travel)
+        if command in "nsew":
+            dir_of_travel = command
+            new_roomnum = exithandler(player["location"], dir_of_travel)
             # Handle invalid exit
             if new_roomnum == -1:
                 print("You cannot go in that direction.")
             # Handle closed doors
-            elif room[roomnum][dir_of_travel]["isclosed"]:
+            elif room[player["location"]][dir_of_travel]["isclosed"]:
                 print("The door is closed.")
             # Set the next room and proceed to the next loop.
             else:
-                roomnum = new_roomnum
+                player["location"] = new_roomnum
                 continue_loop = True
 
-        if keyboardinput.lower() in "x":
+        if command in "x":
             continue_loop = True
