d6d1f07e33e93c8c9f97a8ce3a64a49f079b0929
John Madon <john@unwisegeek.net>
2020-12-25 02:52:01 -0500

    Made rooms.load_map function to leverage files made by mapeditor.

--- rooms.py
+++ rooms.py
@@ -4,6 +4,16 @@ def room_num_exists(room, new_number):
             return True
     return False
 
+def load_map(filename="default.map"):
+    try:
+        file = open(filename, 'r')
+    except:
+        raise Exception("Could not open {} for reading.".format(filename))
+    else:
+        room = json.loads(file.read())
+        return room
+    return []
+
 # RoomNum, RoomName, RoomDescription, North, South, East, West
 room_template = {
             "num": -1,
@@ -13,50 +23,3 @@ room_template = {
             "s": {"leadsto": -1, "isclosed": False, "islocked": False},
             "e": {"leadsto": -1, "isclosed": False, "islocked": False},
             "w": {"leadsto": -1, "isclosed": False, "islocked": False}}
-
-room = [{
-            "num": 0,
-            "name": "The Main Room",
-            "desc": "This is the first room you will see.",
-            "n": {"leadsto":  1, "isclosed": False, "islocked": False},
-            "s": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "e": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "w": {"leadsto": -1, "isclosed": False, "islocked": False}
-        },
-        {
-            "num": 1,
-            "name": "The Second Room",
-            "desc": "This is the second room you will see.",
-            "n": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "s": {"leadsto":  0, "isclosed": False, "islocked": False},
-            "e": {"leadsto":  2, "isclosed": False, "islocked": False},
-            "w": {"leadsto": -1, "isclosed": False, "islocked": False}
-        },
-        {
-            "num": 2,
-            "name": "The Third Room",
-            "desc": "This is the third room you will see.",
-            "n": {"leadsto":  3, "isclosed": False, "islocked": False},
-            "s": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "e": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "w": {"leadsto":  1, "isclosed": False, "islocked": False}
-        },
-        {
-            "num": 3,
-            "name": "The Fourth Room",
-            "desc": "This is the fourth room you will see.",
-            "n": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "s": {"leadsto":  2, "isclosed": False, "islocked": False},
-            "e": {"leadsto":  4, "isclosed": True, "islocked": True},
-            "w": {"leadsto": -1, "isclosed": False, "islocked": False}
-        },
-        {
-            "num": 4,
-            "name": "The Fifth Room",
-            "desc": "This is the fifth room you will see.",
-            "n": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "s": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "e": {"leadsto": -1, "isclosed": False, "islocked": False},
-            "w": {"leadsto":  3, "isclosed": False, "islocked": True}
-        },
-]
